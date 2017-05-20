f76ce7068197669f48df79fbf7a990cc6ac170ce
Chris Hoffman <choffman@optoro.com>
2017-05-20 13:58:24 -0400

    This is not important for this project.
    Also, it's not what my editor does by default.

--- app/controllers/admin/scoreboard_controller.rb
+++ app/controllers/admin/scoreboard_controller.rb
@@ -7,12 +7,12 @@ module Admin
 
     private
 
-      def scoreboard_params
-        params.permit(:contest_id, :logged_in)
-      end
+    def scoreboard_params
+      params.permit(:contest_id, :logged_in)
+    end
 
-      def active_teams
-        ActiveTeamsService.new(contest: @contest, teams: @contest.teams).call
-      end
+    def active_teams
+      ActiveTeamsService.new(contest: @contest, teams: @contest.teams).call
+    end
   end
 end
